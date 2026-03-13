bb9adae7e5109e21fc21ee9ca739305f7a476b48
Brian2994 <brianpari29@gmail.com>
2026-03-12 21:16:03 -0300

    feat: create initial migrations for gateways, transactions and related tables

---
+++ database/migrations/1773341254015_create_transactions_table.ts
@@ -0,0 +1,26 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'transactions'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('client_id').unsigned().references('id').inTable('clients')
+      table.integer('gateway_id').unsigned().references('id').inTable('gateways')
+
+      table.string('external_id')
+      table.string('status')
+      table.integer('amount')
+      table.string('card_last_numbers')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
