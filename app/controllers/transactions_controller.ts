ba56b8fc5b1386af8866e0f5d6f55d38dc5cc2ff
Brian2994 <brianpari29@gmail.com>
2026-03-14 01:30:02 -0300

    feat: create TransactionsController for handling purchase flow

---
+++ app/controllers/transactions_controller.ts
@@ -0,0 +1,76 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Product from '#models/product'
+import Client from '#models/client'
+import Transaction from '#models/transaction'
+import TransactionProduct from '#models/transaction_product'
+import PaymentService from '#services/payment_service'
+import Gateway from '#models/gateway'
+
+export default class TransactionsController {
+
+    async purchase({ request, response }: HttpContext) {
+
+        const { name, email, cardNumber, cvv, products } = request.only([
+            'name',
+            'email',
+            'cardNumber',
+            'cvv',
+            'products'
+        ])
+
+        let total = 0
+        // 1. Cálculo do total
+        for (const item of products) {
+
+            const product = await Product.findOrFail(item.product_id)
+
+            total += product.amount * item.quantity
+        }
+        // 2. Garante o cliente no banco
+        const client = await Client.firstOrCreate(
+            { email },
+            { name, email }
+        )
+
+        const paymentService = new PaymentService()
+        // 3. Processa o pagamento via Service (Failover interno)
+        const payment = await paymentService.processPayment({
+            amount: total,
+            name,
+            email,
+            cardNumber,
+            cvv
+        })
+        // 4. Cria a transação (Ajustado de 'gateway' para 'gatewayId')
+        const gateway = await Gateway
+            .query()
+            .where('name', payment.gateway)
+            .firstOrFail()
+
+        const transaction = await Transaction.create({
+            clientId: client.id,
+            gatewayId: gateway.id,
+            externalId: payment.response.id,
+            status: 'success',
+            amount: total,
+            cardLastNumbers: cardNumber.slice(-4)
+        })
+        // 5. Registra os produtos da transação
+        for (const item of products) {
+
+            await TransactionProduct.create({
+                transactionId: transaction.id,
+                productId: item.product_id,
+                quantity: item.quantity
+            })
+
+        }
+
+        return response.ok({
+            message: 'Payment successful',
+            transaction
+        })
+
+    }
+
+}
